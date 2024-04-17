6f786c22dec53f2f839207114883de6dfd58b3a9
théo <theo.collin69@gmail.com>
2024-04-17 16:28:36 +0200

    ajout relative path dans main.js + vue.d.ts

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.js'
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
-import { createApp } from 'vue'
+import { createApp } from '../node_modules/vue/dist/vue.d.ts'
 import App from './App.vue'
 
 
