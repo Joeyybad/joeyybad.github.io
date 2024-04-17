29da041923e64a283325013bed5cb82026ef9f56
théo <theo.collin69@gmail.com>
2024-04-17 15:44:29 +0200

    Correction slider avec bootstrap 5.3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,14 +11,14 @@
                 <img src="../assets/image/b-boy-mathis-red-bull-bc-one-camp-france-2022.avif" class="d-block w-100" alt="...">
             </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
 </template>
 
